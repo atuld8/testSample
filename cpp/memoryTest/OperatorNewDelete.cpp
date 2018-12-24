d66bc6107a86d1ca71057d5a49aaeddfde07c5b5
Atul Das <atul.das1@veritas.com>
2018-12-24 15:38:39 +0530

    Added new test cpp sample to check new and delete operator overloading

---
+++ cpp/memoryTest/OperatorNewDelete.cpp
@@ -0,0 +1,106 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  OperatorNewDelete.cpp
+ *
+ *    Description:  The sample program to test the new and delete operator
+ *
+ *        Version:  1.0
+ *        Created:  12/23/18 21:06:19
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Atul Das (ad), atuldas.ind@gmail.com
+ *   Organization:  Anonymus private company ltd.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CMemoryManager {
+
+    public:
+        void * operator new (size_t size) {
+            cout << "CMemoryManager: new operator call" << endl;
+
+            if (size <= 0)
+                return NULL;
+
+            return (void *) malloc(size);
+        }
+
+        void operator delete (void * pointer) {
+            cout << "CMemoryManager: delete operator call" << endl;
+
+            if (pointer)
+                free(pointer);
+        }
+};
+
+class CStudent : public CMemoryManager {
+    public:
+        CStudent (char * name, int age)
+            :   _name(name),
+                _age(age) {
+            cout << "CStudent C'tor" << endl;
+        }
+
+        void display () {
+            cout << "Name: " << _name << " Age: " << _age << endl;
+        }
+
+    private:
+        char * _name;
+        int    _age;
+};
+
+class CEmployee {
+    public:
+        CEmployee (string name, int experience)
+            : _name (name),
+              _experience (experience) {
+            cout << "CEmployee C'tor" << endl;
+       }
+
+        void display () {
+            cout << "Name: " << _name << " Experience: " << _experience << endl;
+        }
+
+    private:
+        string  _name;
+        int     _experience;
+};
+
+void * operator new (size_t size) {
+     cout << "Global new operator" << endl;
+
+     if (size <= 0)
+         return NULL;
+
+     return (void *) malloc (size);
+}
+
+void operator delete (void * pointer) {
+     cout << "Global delete operator" << endl;
+    if (pointer)
+        free (pointer);
+}
+
+
+int main() {
+    CStudent  * pStudent  = new CStudent ("student1", 18);
+    CEmployee * pEmployee = new CEmployee ("Employee1", 10);
+
+    pStudent->display ();
+    pEmployee->display ();
+
+    delete (pStudent);
+    delete (pEmployee);
+
+    return 0;
+}
