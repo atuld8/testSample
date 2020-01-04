e7e4ad804ad9c84f6365993c12d726241ab05fe2
atuld8 <atuld8@gmail.com>
2020-01-04 19:51:22 +0530

    handled d'ctor issue and added note about the singleton file

--- cpp/pattern/SingaltonPattern.cpp
+++ cpp/pattern/SingaltonPattern.cpp
@@ -1,6 +1,6 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 
-   * File Name : SingaltonPattern.cpp
+   * File Name : SingletonPattern.cpp
 
    * Purpose :
 
@@ -8,18 +8,26 @@
 
    * Last Modified : Mon Mar  2 07:46:55 2015
 
+   * Singleton class properties
+   *    - private static variable
+   *    - private contructor, copy contructor, assignment operator
+   *    - public destructor
+   *    - public static getInstance method
    _._._._._._._._._._._._._._._._._._._._._.*/
 #include <stdio.h>
 #include <iostream>
 
-class SingaltonPattern {
+class SingletonPattern {
 public:
-        static SingaltonPattern * GetInstance() {
-            // for thread safe Singalton, put double check of null like below
+        virtual ~SingletonPattern() {
+        }
+
+        static SingletonPattern * GetInstance() {
+            // for thread safe Singleton, put double check of null like below
             // if (s_pSp == NULL )
             // check_semaphore call to lock the mutex, the again check to no conflicts
             if (s_pSp == NULL )
-                s_pSp = new SingaltonPattern();
+                s_pSp = new SingletonPattern();
 
             return s_pSp;
         }
@@ -29,36 +37,28 @@ public:
         void setData(int idata) { m_data = idata; }
 
 protected:
-        SingaltonPattern(): m_data(0) { 
+        SingletonPattern(): m_data(0) {
         }
 
-        SingaltonPattern(const SingaltonPattern& sp):m_data(sp.m_data) { 
+        SingletonPattern(const SingletonPattern& sp):m_data(sp.m_data) {
         }
 
-        virtual ~SingaltonPattern() { 
-            if (s_pSp != NULL) 
-            {
-                delete s_pSp;
-                s_pSp = NULL;
-            }
-        }
-
-        SingaltonPattern & operator= (const SingaltonPattern& sp) {
+        SingletonPattern & operator= (const SingletonPattern& sp) {
             return (*this);
         }
 
 private:
-        static  SingaltonPattern* s_pSp;
+        static  SingletonPattern* s_pSp;
         int     m_data;
 };
 
-SingaltonPattern * SingaltonPattern::s_pSp = NULL;
+SingletonPattern * SingletonPattern::s_pSp = NULL;
 
 int main() {
-    SingaltonPattern::GetInstance()->setData(5);
-    printf("The value set in SingaltonPattern is %d\n", SingaltonPattern::GetInstance()->getData());
+    SingletonPattern::GetInstance()->setData(5);
+    printf("The value set in SingletonPattern is %d\n", SingletonPattern::GetInstance()->getData());
 
-    SingaltonPattern::GetInstance()->setData(2);
-    printf("The value set in SingaltonPattern is %d\n", SingaltonPattern::GetInstance()->getData());
+    SingletonPattern::GetInstance()->setData(2);
+    printf("The value set in SingletonPattern is %d\n", SingletonPattern::GetInstance()->getData());
     return 0;
 }
